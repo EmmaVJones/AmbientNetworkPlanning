590efce6b00d8f2855d595495fbb1e22d5df649a
Jones <Emma.Jones@deq.virginia.gov>
2017-10-17 11:39:44 -0400

    working app

--- global.R
+++ global.R
@@ -1,5 +1,52 @@
 library(shiny)
 library(shinyjs)
 library(mapview)
-library(leaflet)
-library(raster)
+library(leaflet) # using development version of leaflet to get legend group options devtools::install_github('rstudio/leaflet')
+library(tidyverse)
+library(rgdal)
+library(DT)
+
+BRRO_sites <- readRDS('data/BRROdata2011to2016_sites.RDS')
+BRRO <- readRDS('data/BRROdata2011to2016.RDS')
+HUCS <- readOGR('data','BRRO')
+
+
+
+# Map 
+stationMapUI <- function(id){
+  ns <- NS(id)
+  tagList(
+      leafletOutput(ns("map"), height = 800))
+}
+
+stationMap <- function(input, output, session){
+  ns <- session$ns
+  
+  pal <- colorFactor(c("blue","firebrick","ghostwhite","darkseagreen","limegreen","yellow","aquamarine","red",
+                       "grey50","midnightblue","magenta",'mediumpurple4',"moccasin","sienna"),levels=unique(BRRO_sites$FDT_SPG_CODE), ordered=T)
+  
+  output$map <- renderLeaflet({
+    #require(gageLocation())
+    
+    leaflet(BRRO_sites) %>%
+      addProviderTiles(providers$Thunderforest.Landscape,group='Thunderforest Landscape')%>%
+      addProviderTiles(providers$Esri.WorldImagery,group='Esri World Imagery')%>%
+      addProviderTiles(providers$OpenStreetMap,group='Open Street Map')%>%
+      addProviderTiles(providers$Stamen.TerrainBackground,group='Stamen Terrain Background')%>%
+      addCircleMarkers(data=BRRO_sites,~Longitude,~Latitude,radius=5,color=~pal(FDT_SPG_CODE),#fillColor="blue",
+                       fillOpacity = 1,stroke=0,
+                       group="Stations",layerId=~FDT_STA_ID,
+                       popup=popupTable(BRRO_sites))%>%
+      addPolygons(data=HUCS,color='gray',fill=0.1,stroke=0.2,group="Watersheds",
+                  popup=paste(sep='<br/>',
+                              paste("HUC6: ",HUCS@data$VAHU6,sep=""),HUCS@data$VaName))%>%hideGroup('Watersheds')%>%
+      addLegend(pal = pal, values = ~FDT_SPG_CODE, opacity = 1,group='Stations')%>%
+      addLayersControl(baseGroups=c('Thunderforest Landscape','Esri World Imagery',
+                                    'Open Street Map','Stamen Terrain Background'),
+                       overlayGroups=c('Stations','Watersheds'),
+                       options=layersControlOptions(collapsed=T),
+                       position='topleft')%>%
+      addMiniMap(tiles = providers$OpenStreetMap,toggleDisplay = TRUE)%>%
+      mapview::addMouseCoordinates(style='basic')
+  })
+}
--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 source('global.R')
+source('multipleDependentSelectizeArguments.R')
 
 
 load_data <- function() {
@@ -7,7 +8,6 @@ load_data <- function() {
   shinyjs::show("main_content")
 }
 
-BRRO <- readRDS('data/BRROdata2011to2016.RDS')
 
 
 shinyServer(function(input, output, session) {
@@ -15,5 +15,22 @@ shinyServer(function(input, output, session) {
   # display the loading feature until data
   load_data()
   
+  # Query By Selectize arguments
+  the_data <- reactive({BRRO_sites})
+  
+  level1_filter <- shiny::callModule(dynamicSelect, "level1", the_data, "STA_LV1_CODE" )
+  level3_filter <- shiny::callModule(dynamicSelect, "level3", level1_filter, "STA_LV3_CODE" )#level1_filter()$STA_LV3_CODE)
+  basin_filter <- shiny::callModule(dynamicSelect, "basin", level3_filter, "Basin")#level3_filter()$Basin)
+  watershed_filter <- shiny::callModule(dynamicSelect, "watershed", basin_filter, "Huc6_Vahu6")#year_filter()$Basin)
+  
+  stationID_filter <- shiny::callModule(dynamicSelect, "stationID", the_data, "FDT_STA_ID" )
+  
+  
+  output$selectedElementsTable <- DT::renderDataTable({
+    
+      datatable(watershed_filter())
+    })
+  
+  callModule(stationMap, "stationMap")
   
 })
